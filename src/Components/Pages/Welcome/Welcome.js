769ff3627aabbb383b5437c7159f467e9288980a
muzaahmadk2 <muzaahmadk2@gmail.com>
2023-06-27 22:19:51 +0400

    email verification send

--- src/Components/Pages/Welcome/Welcome.js
+++ src/Components/Pages/Welcome/Welcome.js
@@ -1,8 +1,45 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { Route, Link } from "react-router-dom";
 import "./Welcome.css";
+import { Button } from "react-bootstrap";
+import AuthContext from "../../Store/Auth-Context";
+
 
 function Welcome() {
+  const authCtx = useContext(AuthContext);
+ 
+  const verifyEmailHandler = () => {
+
+    let url =
+    "https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyB9ZONkhe98ETh530TgqVIj63rXXVIWPDs";
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        requestType: 'VERIFY_EMAIL',
+        idToken: authCtx.token,
+      }),
+      header: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          return res.json().then((data) => {
+            let errorMessage = data.error.message;
+            throw new Error(errorMessage);
+          });
+        }
+      })
+      .then((data) => {
+        alert("Email Verification Send");
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  }
+
   return (
     <div className="welcome">
       <span >
@@ -11,6 +48,9 @@ function Welcome() {
       <span >
         Your profile is incomplete <Link to="/profile">Complete Now</Link>
       </span>
+      <div className="verify">
+      <Button variant="outline-info"  onClick={verifyEmailHandler}>Verify Your Email</Button>
+      </div>
     </div>
   );
 }
